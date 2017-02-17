f0ef2fd358e206e9d55ba840b39822a9372e9ecc
Adam Bien <abien@adam-bien.com>
2017-02-17 18:32:53 +0100

    Sample / test application with system test implemented

---
+++ tracee-jaxrs-client-st/src/test/java/com/airhacks/zipkin/boundary/TracesResourceIT.java
@@ -0,0 +1,36 @@
+/*
+ */
+package com.airhacks.zipkin.boundary;
+
+import com.airhacks.tracee.boundary.TracEEClientRequestFilter;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class TracesResourceIT {
+
+    private Client client;
+    private WebTarget tut;
+
+    @Before
+    public void init() {
+
+        this.client = ClientBuilder.newBuilder()
+                .register(new TracEEClientRequestFilter("http://localhost:9411"))
+                .build();
+        this.tut = this.client.target("http://localhost:8080/tracee-jaxrs/resources/traces");
+    }
+
+    @Test
+    public void traces() {
+        String result = this.tut.request().get(String.class);
+        System.out.println("result = " + result);
+    }
+
+}
